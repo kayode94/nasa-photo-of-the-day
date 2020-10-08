07638cbbf52a2dc0ce4850865aebc4aaa21fa376
kayode <kayode.richards@gmail.com>
2020-10-08 17:19:26 -0400

    added stylings

--- src/NasaInfo.js
+++ src/NasaInfo.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const MainTextStyles = styled.div`
+p{
+    width:70%;
+    margin-left:20%;
+    font-weight:bold;
+    border: solid gold; 
+    background-color: green;
+}
+
+
+`
 function NasaInfo(props){
     const{explanation} = props
     return(
+        <MainTextStyles>
         <div>
             <p>{explanation}</p>
         </div>
+        </MainTextStyles>
     )
 }
 
--- src/PageTitle.js
+++ src/PageTitle.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const PageTitleStyles = styled.div`
+h1{
+    width:100%;
+    font-weight:bold;
+    border: dotted magenta; 
+    background-color: purple;
+}
+h2{
+    width:90%;
+    font-weight:bold;
+    border: dotted blue;
+    background-color: brown;
+    &:hover{
+        transform:scale(1.5);
+        transition: all 1.5s ease-in-out;
+    }
+}
+`
 
 function PageTitle(props){
     const {title, date} = props
     return(
+        <PageTitleStyles>
         <div>
             <h1>Title: {title}</h1>
             <h2>Publish Date: {date}</h2>
         </div>
+        </PageTitleStyles>
     )
 }
 
