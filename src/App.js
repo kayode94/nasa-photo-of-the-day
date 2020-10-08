1029712474043c38014f87e8e5018d2fc76e939c
kayode <kayode.richards@gmail.com>
2020-10-07 21:46:36 -0400

    still working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
+import {BASE_URL, API_KEY} from './constants'
 import "./App.css";
 
 function App() {
+  const [nasaData, setNasaData] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2015-03-14')
+    .then(response=>{
+      console.log(response)
+      setNasaData(response.data)
+    })
+    .catch(error=>{
+      console.log('THIS IS YOUR ERROR ----->', error)
+    })    
+  }, [])
   return (
     <div className="App">
       <p>
